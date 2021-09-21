8d9999ab9b081063a1901e2b0bfa82cf6b445b3a
akash <akash300383@gmail.com>
2021-09-21 18:28:15 +0530

    done

--- Java Programs/Demo.java
+++ Java Programs/Demo.java
@@ -1,19 +1,39 @@
+//import java.util.Scanner;
+import java.util.*;
 class Demo {
-    public static void main(String args[]){
-        int []abc={10,20,30,40,50};
-        int []num = new int[10];
-        System.out.println("Size of array is "+abc.length);
-        System.out.println("Size of array is "+num.length);
-        System.out.println(abc[0]);
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        int temp =100;
-        for(int i=0;i<num.length;i++){
-            num[i]=temp;
-            temp++;
-        }    
-        for(int i=0;i<num.length;i++){
-            System.out.println(num[i]);
+    public static void main(String args[]){        
+        Scanner obj = new Scanner(System.in);
+        System.out.println("how many number do you want to store?");
+        int n = obj.nextInt();
+        int []id=new int[n];
+        String []name = new String[n];
+        float []salary = new float[n];
+        String []desg=new String[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the id");
+            id[i]=obj.nextInt();
+            System.out.println("Enter the name");
+            name[i]=obj.next();
+            System.out.println("Enter the salary");
+            salary[i]=obj.nextFloat();
+            System.out.println("Enter the Desg");
+            desg[i]=obj.next();
+        }  
+        for(int i=0;i<n;i++){
+            if(desg[i].equals("Manager")){
+                  salary[i]=salary[i]+5000;  
+            }else if(desg[i].equals("Developer")){
+                   salary[i]=salary[i]+3000; 
+            }else {
+                salary[i]=salary[i]+1500;
+            }
+        }
+        System.out.println("All details are");
+        for(int i=0;i<n;i++){
+            System.out.println("id is "+id[i]);
+            System.out.println("name is "+name[i]);
+            System.out.println("salary is "+salary[i]);
+            System.out.println("desg is "+desg[i]);
         }
     }
 }
