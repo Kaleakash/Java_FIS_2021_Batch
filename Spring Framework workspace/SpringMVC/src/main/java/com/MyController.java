15f080af3a4a332eb973f9f8868bfcc61b7067c8
akash <akash300383@gmail.com>
2021-10-05 21:47:34 +0530

    done

--- Spring Framework workspace/SpringMVC/src/main/java/com/MyController.java
+++ Spring Framework workspace/SpringMVC/src/main/java/com/MyController.java
@@ -2,16 +2,26 @@ package com;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller			// this is like a Servlet 
 public class MyController {
 
-	@RequestMapping(value = "hello")
+	@RequestMapping(value = "hello",method = RequestMethod.GET)
 	public ModelAndView sayHello() {
 		System.out.println("I Came Here");					// Service --Dao layer --Dao layer 
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("display.jsp");
 		return mav;
 	}
+	
+	@RequestMapping(value = "hi",method = RequestMethod.POST)
+	public ModelAndView sayHi() {
+		System.out.println("I Came Here");					// Service --Dao layer --Dao layer 
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("displayInfo.jsp");
+		return mav;
+	}
+	
 }
